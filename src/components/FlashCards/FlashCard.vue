b49ed4d1e82c076711088b1ba9be1637072c42a1
dg <dg@twork.local>
2024-02-21 11:39:52 +0900

    Installed vue-swipy. Updated FlashCard.vue to recognize being dragged vs. being clicked.

--- src/components/FlashCards/FlashCard.vue
+++ src/components/FlashCards/FlashCard.vue
@@ -1,81 +1,84 @@
 <script lang="ts">
 export default {
-	name: "FlashCard",
-	data() {
-		return {
-			flipped: false,
-		};
-	},
-	props: {
-		card: Array,
-		labels: Object
-	},
-	methods: {
-		flip() {
-			this.flipped = !this.flipped;
-		},
-	},
-	computed: {
-		getFront() {
-			return this.card.filter(
-				(val, i) => this.labels[i] && this.labels[i].side === "front"
-			);
-		},
-		getBack() {
-			return this.card.filter(
-				(val, i) => this.labels[i] && this.labels[i].side === "back"
-			);
-		},
-	}
-};
+  name: 'FlashCard',
+  data() {
+    return {
+      click: false,
+      drag: false,
+      flipped: false
+    }
+  },
+  props: {
+    card: Array,
+    labels: Object
+  },
+  methods: {
+    flip() {
+      //mouseup fires after dragstart, so it will reset drag status if was set
+      if (this.drag) {
+        this.click = false
+        this.drag = false
+        return
+      }
+
+      this.flipped = !this.flipped
+    }
+  },
+  computed: {
+    getFront() {
+      return this.card.filter((val, i) => this.labels[i] && this.labels[i].side === 'front')
+    },
+    getBack() {
+      return this.card.filter((val, i) => this.labels[i] && this.labels[i].side === 'back')
+    }
+  }
+}
 </script>
 
 <template>
-	<div :class="
-      'display-card p-3 bg-light' +
-      (this.flipped ? ' flipped' : '')
-    "
-		@click="flip"
-	>
-		<div v-for="(label, i) in getFront" :key="i">
-			<span>{{ label }}</span>
-		</div>
-	</div>
-	<div
-		:class="
-      'display-card p-3 bg-neutral-700 text-neutral-200' +
-      (!this.flipped ? ' flipped' : '')
-    "
-		@click="flip"
-	>
-		<div v-for="(label, i) in getBack" :key="i">
-			<span>{{ label }}</span>
-		</div>
-	</div>
+  <div
+    class="parent-display-card"
+    @dragstart="this.drag = true"
+    @mousedown="this.click = true"
+    @mouseup="flip"
+    draggable="true"
+  >
+    <div :class="'display-card p-3 bg-light' + (this.flipped ? ' flipped' : '')">
+      <div v-for="(label, i) in getFront" :key="i">
+        <span>{{ label }}</span>
+      </div>
+    </div>
+    <div
+      :class="
+        'display-card p-3 bg-neutral-700 text-neutral-200' + (!this.flipped ? ' flipped' : '')
+      "
+    >
+      <div v-for="(label, i) in getBack" :key="i">
+        <span>{{ label }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
 .display-card {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	backface-visibility: hidden;
-	position: absolute;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	transition: all 0.8s ease;
-	height: 100%;
-	width: 100%;
-	z-index: 2;
-	grid-column: 1 / 1;
-	grid-row: 1 / 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  backface-visibility: hidden;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  transition: all 0.8s ease;
+  height: 100%;
+  width: 100%;
+  z-index: 2;
 }
 .display-card.flipped {
-	transform: rotateY(180deg);
-	z-index: 1;
+  transform: rotateY(180deg);
+  z-index: 1;
 }
 </style>
